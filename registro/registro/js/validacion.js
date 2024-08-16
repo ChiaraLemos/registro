663a79ff4352db91025db0b8e164463c980019e9
ChiaraLemos <chiaramalemos@gmail.com>
2024-08-16 09:49:11 -0300

    Update validacion.js

--- registro/registro/js/validacion.js
+++ registro/registro/js/validacion.js
@@ -1,7 +1,63 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
+    
 }
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.addEventListener(`DOMContentLoaded`, function() {
+
+
+ const camposTexto = document.getElementsByClassName("form-control"); 
+
+	for (i=0; i < camposTexto.length; i++) {
+		if (camposTexto.value == ) {
+            return " está vacio y es OBLIGATORIO"
+			 } else {
+
+             }
+            
+			
+        }
+}
+)};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const regBtn = document.getElementById('regBtn');
+    
+    regBtn.addEventListener('click', () => {
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const password1 = document.getElementById('password1').value;
+        const password2 = document.getElementById('password2').value;
+        
+        const terminos = document.getElementById('terminos').checked;
+        
+         // Validaciones
+         if (!nombre || !apellido || !email || !password1 || !password2) {
+            showAlertError('Todos los campos deben estar completos.');
+            return;
+        }
+        
+        if (password1.length < 6) {
+            showAlertError('La contraseña debe tener al menos 6 caracteres');
+            return;
+        }
+        
+        if (password1 !== password2) {
+            showAlertError('Las contraseñas no coinciden');
+            return;
+        }
+        
+        if (!terminos) {
+            showAlertError('Debe aceptar los términos y condiciones.');
+            return;
+        }
+        
+        // Si todas las validaciones pasan
+        showAlertSuccess('Datos guardados correctamente');
+    });
+});
